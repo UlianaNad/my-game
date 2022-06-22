9777c3d1a1a9d2f1eeebaf230887a34fb4bd51b4
”John <102276922+UlianaNad@users.noreply.github.com>
2022-06-22 11:15:02 +0300

    change22.06

--- js/main.js
+++ js/main.js
@@ -2,33 +2,79 @@ let mainCont = document.querySelector('.mainconteiner');
 
 let redCube = document.querySelector('.redcube');
 
+let left1 = 0;
+let top1 = 0;
 
-let left = 0;
+// function moveCube(cubeElement, direction, currentPosition, directionSide, defaultMargin = 10) {
+//     let newPosition = 0;
+//     if(directionSide === 'plus'){
+//         newPosition = currentPosition + defaultMargin;
+//     } else if(directionSide === 'minus') {
+//         newPosition = currentPosition - defaultMargin;
+//     }
+//     if(direction === 'right') {
+//         cubeElement.style.left = newPosition + 'px';
 
-window.onkeydown = function moveLeft() {
-    if(event.keyCode == 37) {
-        left = left-10;
-        redCube.style.left = left + 'px';
+//     } else if(direction === 'top') {
+//         cubeElement.style.top = newPosition + 'px';
+//     } 
+//     return newPosition;
+// };
+
+let pressUp = document.querySelector('.pressup');
+
+pressUp.addEventListener('click', () => {
+    if (top1 <= 0) return;
+    redCube.style.top = top1 + 'px';
+    top1 = top1 - 10;
+    });
+
+let pressDown = document.querySelector('.pressdown');
+
+pressDown.addEventListener('click', () => {
+    if (top1 >= 400 ) return;
+     redCube.style.top = top1 + 'px';
+     top1 = top1 + 10;
+    });
+
+let pressRight = document.querySelector('.pressright');
+
+ pressRight.addEventListener('click', () => {
+    console.log(left1);
+    if (left1 >= 900) return;
+    redCube.style.left = left1 + 'px';
+        left1 = left1 + 10;
+    });
+let pressLeft = document.querySelector('.pressleft');
+
+ pressLeft.addEventListener('click', () => {
+    if (left1 <= 0) return;
+    redCube.style.left = left1 + 'px';
+        left1 = left1 - 10;
+    });
+
+document.onkeydown = function (event){
+  
+    if (event.key == 'ArrowRight') {
+       if (left1 >= 900) return;
+       redCube.style.left = left1 + 'px';
+       left1 = left1 + 5;
     }
-    else if (event.keyCode == 39) {
-        left = left+10;
-        redCube.style.left = left + 'px';
+     else if (event.key == 'ArrowDown') {
+        if (top1 >= 400) return;
+       redCube.style.top = top1 + 'px';
+       top1 = top1 + 5;
     }
-
-};
-
-
-
-window.onkeyup = function moveTop() {
-    let top = 0;
-    if(event.keyCode == 38) {
-        top = top-10;
-        redCube.style.top = top + 'px';
+    if (event.key == 'ArrowLeft') {
+        if (left1 <= 0) return;
+        redCube.style.left = left1 + 'px';
+        left1 = left1 - 5;
     }
-    else if (event.keyCode == 40) {
-        top = top+10;
-        redCube.style.top = top + 'px';
-    }
+    else if (event.key == 'ArrowUp') {
+        if (top1 <= 0) return;
+       redCube.style.top = top1 + 'px';
+        top1 = top1 - 5;
+    };
 };
 
 
